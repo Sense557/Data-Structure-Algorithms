3fe7c26a756b62215df0d6cabf680c5980ef8275
Shakti <mehershakti1020@gmail.com>
2024-08-25 22:12:39 +0530

    added Two Sum II - Input Array Is Sorted

---
+++ 1.Arrays/3.TwoSum2InputSorted/Solution.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let n = nums.length;
+    let i = 0;
+    let j = n-1;
+    while (i < j) {
+        if (nums [i] + nums [j] == target) {
+            return [i+1, j+1]; // 0-index -> 1-index
+        } else if (nums [i] + nums [j] > target ) {
+            j--;
+        } else {
+            i++;
+        }
+    }
+};
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Arrays
 - [Two Sum](https://leetcode.com/problems/two-sum/)
 - [Reverse a String](https://leetcode.com/problems/reverse-string/)
+- [Two Sum II - Input Array Is Sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/)
